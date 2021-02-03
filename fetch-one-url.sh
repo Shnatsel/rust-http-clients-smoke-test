475ca73981efb381b4cad3cf457e5cff2e8f651b
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-03 18:10:52 +0100

    Add a wrapper script that provides an additional timeout and output sorting

---
+++ fetch-one-url.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+binary_to_invoke = "$1"
+url = "$2"
+
+timeout --kill-after=70s 60s "$binary_to_invoke" "$url" 2>&1 > "$url"
+exit_code = "$?"
+if ["$exit_code" -ne 0]; then
+    mv "$url" 'success'/"$url"
+else
+    mv "$url" 'failure'/"$url"
+fi

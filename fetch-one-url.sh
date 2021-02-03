bd81abe66e630ae9bb06f7c4bf7f86960bfe2675
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-03 18:11:32 +0100

    Record exit code with wrapper script

--- fetch-one-url.sh
+++ fetch-one-url.sh
@@ -7,6 +7,7 @@ url = "$2"
 
 timeout --kill-after=70s 60s "$binary_to_invoke" "$url" 2>&1 > "$url"
 exit_code = "$?"
+echo "Exit code: $exit_code" >> "$url"
 if ["$exit_code" -ne 0]; then
     mv "$url" 'success'/"$url"
 else

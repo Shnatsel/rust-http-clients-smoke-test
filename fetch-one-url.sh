a144ede7fb38136418617fa2d1f916f1c92879f4
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-03 18:22:49 +0100

    Fix shell script syntax

--- fetch-one-url.sh
+++ fetch-one-url.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-binary_to_invoke = "$1"
-url = "$2"
+binary_to_invoke="$1"
+url="$2"
 
 timeout --kill-after=70s 60s "$binary_to_invoke" "$url" 2>&1 > "$url"
-exit_code = "$?"
+exit_code="$?"
 echo "Exit code: $exit_code" >> "$url"
 if ["$exit_code" -ne 0]; then
     mv "$url" 'success'/"$url"

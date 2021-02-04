dff3221399179822472743cef0af0121563dd107
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-04 22:39:21 +0100

    Remove streaming optimizations from ureq harness and treat input as a string to exercise encoding support

--- ureq-smoke-test/src/main.rs
+++ ureq-smoke-test/src/main.rs
@@ -3,14 +3,13 @@
 // and https://github.com/algesten/ureq/blob/main/examples/smoke-test/main.rs
 
 use std::env;
-use std::io::Read;
 use std::time::Duration;
 
 fn main() {
     match smoke_test() {
-        Ok(()) => println!("\nDid not hang! Success"),
+        Ok(()) => println!("Did not hang! Success"),
         Err(err) => {
-            println!("\nDid not hang! Error: {}", err);
+            println!("Did not hang! Error: {}", err);
             std::process::exit(1);
         }
     }
@@ -33,39 +32,16 @@ fn smoke_test() -> Result<(), Box<dyn std::error::Error>> {
 
     // Print headers
     for header_name in response.headers_names() {
-        println!("Header: {}: {:?}", header_name, response.all(&header_name));
+        for value in response.all(&header_name) {
+            println!("Header: {}: {:?}", header_name, value);
+        }
     }
 
-    // Read the first 16Kb of the body to get an idea of what we've downloaded, ignore the rest.
-    let mut reader = response.into_reader();
-    print_first_n_bytes(&mut reader, 16384)?;
-    // If we don't do this, the rest of the body will never actually be read or maybe even received.
-    drain_reader(&mut reader)?;
+    // This retains the whole body in memory, but tests show that RAM is plentiful, so I've rolled back clever optimizations
+    let text = response.into_string()?;
+    // Print the first 8k chars of the body to get an idea of what we've downloaded, ignore the rest.
+    let first_8k_chars_of_body: String = text.chars().take(8192).collect();
+    println!("{}", first_8k_chars_of_body);
 
-    Ok(())
-}
-
-fn print_first_n_bytes(
-    reader: &mut impl Read,
-    bytes: usize,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut limited_reader = reader.take(bytes as u64);
-    drain_reader_to_stdout(&mut limited_reader)?;
-    Ok(())
-}
-
-/// Reads all data from the provider reader and prints it to stdout
-fn drain_reader_to_stdout(reader: &mut impl Read) -> Result<(), Box<dyn std::error::Error>> {
-    let stdout = std::io::stdout();
-    let mut stdout = stdout.lock();
-    std::io::copy(reader, &mut stdout)?;
-    Ok(())
-}
-
-/// Reads all data from the provided reader and throws it away
-fn drain_reader(reader: &mut impl Read) -> Result<(), Box<dyn std::error::Error>> {
-    for byte in reader.bytes() {
-        byte?;
-    }
     Ok(())
 }

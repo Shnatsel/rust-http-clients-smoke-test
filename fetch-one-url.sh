6b4f6e9a1bd51c3d2ba9d2d9a85160c416b197c6
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2021-02-05 23:15:30 +0100

    Fix test harness missing panics

--- fetch-one-url.sh
+++ fetch-one-url.sh
@@ -3,7 +3,7 @@
 binary_to_invoke="$1"
 url="$2"
 
-timeout --kill-after=70s 60s "$binary_to_invoke" "$url" 2>&1 > "$url"
+timeout --kill-after=70s 60s "$binary_to_invoke" "$url" > "$url" 2>&1
 exit_code="$?"
 echo "Exit code: $exit_code" >> "$url"
 if [ "$exit_code" -eq 0 ]; then

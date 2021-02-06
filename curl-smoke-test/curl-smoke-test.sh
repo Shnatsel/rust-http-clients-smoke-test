8110184cb8bec18febc1ac9cde1fc4d64e14af8b
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2021-02-07 00:42:54 +0100

    add ASAN options to curl test harness

--- curl-smoke-test/curl-smoke-test.sh
+++ curl-smoke-test/curl-smoke-test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 tempfile="$(mktemp)"
+(
+export ASAN_OPTIONS=detect_leaks=0
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5
 curl --connect-timeout 10 --speed-time 30 -s -v --location "$1" > "$tempfile" 2>&1
+)
 curl_exit_code="$?"
 # only retain the first 16k of the output
 head -c 16384 "$tempfile"

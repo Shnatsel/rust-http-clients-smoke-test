1b8fa428d38ab16bcb32a179be9411018a85ef54
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2021-02-07 00:29:48 +0100

    Add timeouts to curl test harness

--- curl-smoke-test/curl-smoke-test.sh
+++ curl-smoke-test/curl-smoke-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tempfile="$(mktemp)"
-curl -s -v --location "$1" > "$tempfile" 2>&1
+curl --connect-timeout 10 --speed-time 30 -s -v --location "$1" > "$tempfile" 2>&1
 curl_exit_code="$?"
 # only retain the first 16k of the output
 head -c 16384 "$tempfile"
